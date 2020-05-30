97f6734c69950c299966c54e177c2e562f530922
Israel Medina <isra2dave@gmail.com>
2020-05-30 20:59:22 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img id="dave_logo" src="./assets/dave_logo.svg">
+    <SocialLinks />
+  </div>
+</template>
+
+<script>
+import SocialLinks from './components/SocialLinks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SocialLinks
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #000000;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#dave_logo {
+  width: 70%
+}
+</style>
