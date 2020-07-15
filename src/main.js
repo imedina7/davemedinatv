9d83a6d92997f044bf4e66b4e625245a9708dee5
Israel Medina <isra2dave@gmail.com>
2020-07-14 23:48:28 -0300

    Changed icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { solidSvgIcons } from '@fortawesome/free-solid-svg-icons'
+import { faYoutube, faInstagram, faInstagramSquare, faTwitter, faTwitterSquare, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const brandsIcons = { faYoutube, faInstagram, faInstagramSquare, faTwitter, faTwitterSquare, faFacebook }
+
+// library.add(solidSvgIcons)
+library.add(brandsIcons)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
