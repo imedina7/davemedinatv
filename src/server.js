b06f4c3ca3edfd9b74f93d41a1566994443cb88e
Israel Medina <isra2dave@gmail.com>
2020-10-07 12:21:41 -0300

    Added server-side logging

--- src/server.js
+++ src/server.js
@@ -2,18 +2,28 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
+
 app.use('/', serveStatic(path.join(__dirname, '../dist')))
+
 const port = process.env.PORT || 8080
 
 const liveUrl = process.env.LIVE_YOUTUBE_URL || null
 const liveTimestamp = process.env.LIVE_TIMESTAMP || null
 
 app.use('/api/liveStatus', (req, res, next) => {
+    console.log('*** liveStatusFetch REQUEST ***')
     console.log(req)
-    if (liveUrl && liveTimestamp) {
+
+    if (liveUrl) {
         const jsonResponse = { liveUrl, liveTimestamp }
-        res.send(JSON.stringify(jsonResponse))
+        const jsonStringResponse = JSON.stringify(jsonResponse)
+
+        console.log('*** liveStatusFetch RESPONSE ***')
+        console.log(jsonResponse)
+        res.send(jsonStringResponse)
     } else {
+        console.log('*** liveStatusFetch RESPONSE ***')
+        console.log('*** 404 NOT FOUND ***')
         res.send(404)
     }
 
