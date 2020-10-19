03dc44494c9203999467369044829daa9e55cbe3
Israel Medina <isra2dave@gmail.com>
2020-10-19 20:10:11 -0300

    Fix redis

--- src/lib/clients/redis.js
+++ src/lib/clients/redis.js
@@ -2,7 +2,7 @@ const envars = require('../../config')
 const { init, set, get } = require('node-cache-redis')
 
 const initialize = () => {
-    init({ redisOptions: { redisUrl: envars.REDIS_URL } })
+    return init({ redisOptions: { url: envars.REDIS_URL } })
 }
 
 module.exports = { initialize, set, get }
