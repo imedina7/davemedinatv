bcf79b900b878c3326f502c72aca510803a58ad0
Israel Medina <isra2dave@gmail.com>
2020-10-26 14:34:46 -0300

    added live url redirection

--- src/lib/utils/redir.js
+++ src/lib/utils/redir.js
@@ -1,20 +1,22 @@
 const Click = require('../models/Click')
+const liveUrl = require('../../config').LIVE_YOUTUBE_URL
 
 const ExpressRouter = require('express').Router
 const URL = require('url').URL
 const redis = require('../clients/redis')
 
-function redirect (res, redirUrl) {
-  res.writeHead(301, { Location: redirUrl })
+function redirect(res, redirUrl, next) {
+  if (redirUrl === undefined || redirUrl === null) {
+    res.writeHead(301, { Location: '/' })
+  } else {
+    res.writeHead(301, { Location: redirUrl })
+  }
+  next()
 }
 
 const redir = (req, res, next) => {
   const query = req.query
   const redirUrl = query.url
-
-  if (redirUrl === undefined) {
-    redirect(res, '/', next)
-  }
 
   const refId = query.rid
   const click = new Click(refId, redirUrl)
@@ -22,16 +24,18 @@ const redir = (req, res, next) => {
   redis.saveClick(click).then(() => {
     redirect(res, redirUrl, next)
   }).catch((err) => {
-    redirect(res, redirUrl, next)
     console.error('[!] UX error: ', err)
+    redirect(res, redirUrl, next)
   })
 }
-
+const live = (req, res, next) => {
+  redirect(res, liveUrl, next)
+}
 const Router = () => {
   const router = new ExpressRouter()
 
   router.get('/redir', redir)
-
+  router.get('/live', live)
   return router
 }
 
